aabaa4eca8077db47c3de50cf898c642457d85e1
ambArmando <armando.ambrus@student.upt.ro>
2022-01-15 18:23:45 +0200

    L2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Loloelo',
+            description: 'unguru bulan si berea'
+        }
+    }
+})
