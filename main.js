4d4666e77a7adeecac778a20e1e3f84c8f7b42de
ambArmando <armando.ambrus@student.upt.ro>
2022-01-17 20:07:59 +0200

    L6

--- main.js
+++ main.js
@@ -11,5 +11,19 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+
+        changeColor(variant) {
+            this.image = variant;
+        },
+
+        removeFromCart() {
+            this.cart -= 1;
+        }
     }
 })
+
